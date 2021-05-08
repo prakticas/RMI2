3e4a29248fa8bf8fd4d9396d8fc82e1d8616190f
dolansete <dolancc00@gmail.com>
2021-05-09 01:10:35 +0200

    dar hora y dar fecha hechas

--- ServerA.java
+++ ServerA.java
@@ -1,10 +1,31 @@
 import java.rmi.RemoteException;
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 public class ServerA {
     public String dar_hora() throws RemoteException
     {
-        return "";
+        LocalDateTime lD = LocalDateTime.now();
+        int horas  = lD.getHour();
+        int minutos = lD.getMinute();
+        int segundos = lD.getSecond();
+        return horas  + ":"+ minutos + ":" + segundos; 
     }
     public String dar_fecha() throws RemoteException{
-        return "";
+        Calendar c = new GregorianCalendar();
+        String dia = Integer.toString(c.get(Calendar.DATE));
+        String mes = Integer.toString(c.get(Calendar.MONTH)+1); //+1 porque sino cuenta desde 0
+        String anyo = Integer.toString(c.get(Calendar.YEAR));
+        return dia+"/"+mes+"/"+anyo;
+    }
+    public static void main(String[] args) {
+        try{
+        ServerA sa = new ServerA();
+        String dia = sa.dar_fecha();
+        System.out.println(dia);
+        String hora = sa.dar_hora();
+        System.out.println(hora);
+        }
+        catch(Exception e){}
     }
 }
