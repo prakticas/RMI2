4afa17d8b3d1e7ee0ff083d7047fcfbb73847819
Íñigo Aréjula Aísa <arejula27@gmail.com>
2021-05-20 19:18:14 +0200

    acabado script

--- launch.sh
+++ launch.sh
@@ -1,14 +1,21 @@
+#!/bin/bash
 USER="a785370"
 SERVERLOG="serverA.log"
 BROKERLOG="broker.log"
 WORKINGDIR="/home/a785370/broker"
 JAVAC="/usr/local/java/bin/javac"
 JAVA="/usr/local/java/bin/java"
+servers=(155.210.154.202 155.210.154.203 155.210.154.204)
+IP=${servers[0]}
 
-SERVER=155.210.154.202
-if [ $# -ne 1 ];then
+if [ $# -eq 0 ];then
      echo "inidique el progrma a lanzar"
     exit 85
 fi
 
-ssh -n ${USER}@${SERVER} "cd ${WORKINGDIR};${JAVA} -cp  ${WORKINGDIR}/build $1"
+if [ $# -eq 2 ];then
+     IP=$2
+    
+fi
+
+ssh -n ${USER}@${IP} "cd ${WORKINGDIR};${JAVA} -cp  ${WORKINGDIR}/build $1"
