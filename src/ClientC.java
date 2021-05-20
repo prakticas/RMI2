c2e5a1b224953d8a7a4f70b1e88f3becd038a2d8
Íñigo Aréjula Aísa <arejula27@gmail.com>
2021-05-20 16:28:04 +0200

    bug no encuentra al broker el cliente

--- src/ClientC.java
+++ src/ClientC.java
@@ -4,6 +4,7 @@ public class ClientC {
     public static void main(String[] args){
         //Fijar el directorio donde se encuentra el java.policy
         System.setProperty("java.security.policy", "../configuration/java.policy");
+        String brokerName = "Brokerini";
         if (System.getSecurityManager() == null) {
             //Crear administrador de seguridad
             System.setSecurityManager(new SecurityManager());
@@ -11,7 +12,7 @@ public class ClientC {
         try {
             
             String hostname = "127.0.0.1"; //se puede usar "IP:puerto"
-            Broker broker = (Broker) Naming.lookup("//"+ hostname + "/Broker");
+            Broker broker = (Broker) Naming.lookup("//"+ hostname + "/"+brokerName);
        
             Respuesta today = broker.ejecutar_servicio("dar_fecha",null);
             Respuesta thisHour = broker.ejecutar_servicio("dar_hora",null);
