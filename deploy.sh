aff635029ba567b3912c35a3c93d910ae240dc67
Íñigo Aréjula Aísa <arejula27@gmail.com>
2021-05-20 18:26:00 +0200

    scrpits para cada elemento

--- deploy.sh
+++ deploy.sh
@@ -10,12 +10,18 @@ JAVA="/usr/local/java/bin/java"
 
 servers=(155.210.154.202 155.210.154.203 155.210.154.204)
 
+
 scp   -r configuration ${USER}@central.cps.unizar.es:${WORKINGDIR}
 #ssh -n ${USER}@${servers[0]} "chmod ${WORKINGDIR}/configuration/java.policy g+w"
 scp   -r src ${USER}@central.cps.unizar.es:${WORKINGDIR}
+
+#eliminar instancias antiguas"
+for s in ${servers[@]}; do
+ ssh -n ${USER}@${s} "killall /usr/local/java/bin/java" 2>/dev/null 1>&2 
+done
 ssh -n ${USER}@central.cps.unizar.es "find ${WORKINGDIR}/src -iname \"*.java\" > ${WORKINGDIR}/sources.txt ;${JAVAC} @${WORKINGDIR}/sources.txt -d ${WORKINGDIR}/build"
 
 
 ssh -n ${USER}@${servers[0]} "cd ${WORKINGDIR};${JAVA} -cp  ${WORKINGDIR}/build BrokerImpl"
 #echo ${USER}@${servers[1]}
-#echo ${USER}@${servers[2]}
+#echo ${USER}@${servers[2]}BrokerImpl
